6a1ab69cdc6f8529d1132c4fc367de382caf0e9c
Vada <denis.le01@mail.ru>
2024-04-26 17:07:21 +0300

    create migration manager

---
+++ app/migration_manager.py
@@ -0,0 +1,19 @@
+from app.migrations.create_description_table import CreateDescriptionTable
+from app.migrations.create_ranks_table import CreateRanksTable
+from app.migrations.create_sample_table import CreateSampleTable
+from app.migrations.create_taxonomy_table import CreateTaxonomyTable
+from app.migrations.migration import Migration
+
+
+class MigrationManager:
+    @staticmethod
+    def run_migrations():
+        CreateSampleTable.run()
+        CreateDescriptionTable.run()
+        CreateTaxonomyTable.run()
+        CreateRanksTable.run()
+
+    @staticmethod
+    def refresh():
+        Migration.drop_all_tables()
+        MigrationManager.run_migrations()
