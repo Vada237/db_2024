20ac6a3ad37a6811d0a08d05c7f155c0eec4556b
Vada <denis.le01@mail.ru>
2024-04-27 17:19:08 +0300

    relocate parser manager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app.migration_manager import MigrationManager
 from app.migrations.migration import Migration
-from app.parser.parser_manager import ParserManager
+from app.parser_manager import ParserManager
 from settings import enable_migration, need_refresh, enable_parser
 
 
