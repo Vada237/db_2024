d8e2157b79f8ac251e6834ebe1ed8c34ad06af0f
Vada <denis.le01@mail.ru>
2024-04-27 14:56:55 +0300

    use parser

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from app.migration_manager import MigrationManager
 from app.migrations.migration import Migration
-from settings import enable_migration, need_refresh
+from app.parser.parser_manager import ParserManager
+from settings import enable_migration, need_refresh, enable_parser
 
 
 def main():
     Migration.init()
+    parser_manager = ParserManager()
+
     if enable_migration:
         MigrationManager.run_migrations()
     elif need_refresh:
         MigrationManager.refresh()
+
+    if enable_parser:
+        parser_manager.start()
 
 
 if __name__ == '__main__':
