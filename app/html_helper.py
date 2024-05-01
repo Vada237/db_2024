b0c63f35b803643cac4a85ae95d9046e8f625538
Vada <denis.le01@mail.ru>
2024-05-02 01:00:10 +0300

    update html helper

--- app/html_helper.py
+++ app/html_helper.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from settings import ROW_COUNT
 
 
@@ -8,3 +10,12 @@ class HtmlHelper:
             'backward': True if page > 0 else False,
             'forward': True if (page + 1) * ROW_COUNT < count_rows else False
         }
+
+    @staticmethod
+    def numbers_to_float(number: str) -> Union[int, float, None]:
+        return None if number == '' or number == 'None' else float(number)
+
+    @staticmethod
+    def str_null_to_none(string: str) -> Union[str, None]:
+        if string == 'None':
+            return None
